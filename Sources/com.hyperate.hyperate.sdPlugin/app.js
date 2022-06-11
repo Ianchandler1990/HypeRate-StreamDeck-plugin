75fce6683d8e4f00b21da4f1bf15c36527716ad8
IanChandler <cell600@hotmail.com>
2022-06-11 02:10:35 +0100

    V1.04
    Ready for Store push.

--- Sources/com.hyperate.hyperate.sdPlugin/app.js
+++ Sources/com.hyperate.hyperate.sdPlugin/app.js
@@ -3,187 +3,190 @@ var apikey = config.SECRET_API_KEY;
 var hrimage = config.HRimg;
 var id = "";
 var payload
+
 function enableHypeRate() {
-    const hypeRateURL =
-      "wss://app.hyperate.io/socket/websocket?token=" + apikey;
-    window.hypeRateSocket = new WebSocket(hypeRateURL);
-    // Set hearbeat interval on connect
-    window.hypeRateSocket.onopen = () => {
-      console.log(`HypeRate WebSocket - Connected`);
-      setInterval(function () {
-        hypeRateSocket.send(
-          JSON.stringify({
-            topic: "phoenix",
-            event: "heartbeat",
-            payload: {},
-            ref: 0,
-          })
-        );
-      }, 10000);
-      connectToHypeRate(id);
-    };
-    // Handle incoming message
-    window.hypeRateSocket.onmessage = (event) => {
-      let eventData = JSON.parse(event.data);
-      if (eventData.event !== "hr_update") {
-        return;
-      }
-      $SD.api.setTitle(payload, eventData.payload.hr);
-      console.log(eventData.payload.hr);
-    };
-    // Reconnect on disconnect
-    window.hypeRateSocket.onclose = function (event) {
-      console.error(`HypeRate Error: ${event.code} ${event.reason}`);
-      enableHypeRate();
-    };
-  }
-  async function connectToHypeRate(sessionID) {
-    console.log(`HypeRate - Joining ${sessionID}`);
-    joinHypeRateChannel();
-  }
-  // Join the HypeRate Id on WebSocket
-  async function joinHypeRateChannel() {
-    window.hypeRateSocket.send(
-      JSON.stringify({
-        topic: `hr:${id}`,
-        event: "phx_join",
-        payload: {},
-        ref: 0,
-      })
-    );
-  }
+  const hypeRateURL =
+    "wss://app.hyperate.io/socket/websocket?token=" + apikey;
+  window.hypeRateSocket = new WebSocket(hypeRateURL);
+  // Set hearbeat interval on connect
+  window.hypeRateSocket.onopen = () => {
+    console.log(`HypeRate WebSocket - Connected`);
+    setInterval(function () {
+      hypeRateSocket.send(
+        JSON.stringify({
+          topic: "phoenix",
+          event: "heartbeat",
+          payload: {},
+          ref: 0,
+        })
+      );
+    }, 10000);
+    connectToHypeRate(id);
+  };
+  // Handle incoming message
+  window.hypeRateSocket.onmessage = (event) => {
+    let eventData = JSON.parse(event.data);
+    if (eventData.event !== "hr_update") {
+      return;
+    }
+    $SD.api.setTitle(payload, eventData.payload.hr);
+    console.log(eventData.payload.hr);
+  };
+  // Reconnect on disconnect
+  window.hypeRateSocket.onclose = function (event) {
+    console.error(`HypeRate Error: ${event.code} ${event.reason}`);
+    enableHypeRate();
+  };
+}
+async function connectToHypeRate(sessionID) {
+  console.log(`HypeRate - Joining ${sessionID}`);
+  joinHypeRateChannel();
+}
+// Join the HypeRate Id on WebSocket
+async function joinHypeRateChannel() {
+  window.hypeRateSocket.send(
+    JSON.stringify({
+      topic: `hr:${id}`,
+      event: "phx_join",
+      payload: {},
+      ref: 0,
+    })
+  );
+}
 
 $SD.on('connected', (jsonObj) => connected(jsonObj));
 
 function connected(data) {
-    // Subscribe to the willAppear and other events
-    $SD.on('com.hyperate.hyperate.heartrate.willAppear', (jsonObj) => action.onWillAppear(jsonObj));
-    $SD.on('com.hyperate.hyperate.heartrate.keyUp', (jsonObj) => action.onKeyUp(jsonObj));
-    $SD.on('com.hyperate.hyperate.heartrate.sendToPlugin', (jsonObj) => action.onSendToPlugin(jsonObj));
-    $SD.on('com.hyperate.hyperate.heartrate.didReceiveSettings', (jsonObj) => action.onDidReceiveSettings(jsonObj));
-    $SD.on('com.hyperate.hyperate.heartrate.propertyInspectorDidAppear', (jsonObj) => {
-        console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js]propertyInspectorDidAppear:');
-    });
-    $SD.on('com.hyperate.hyperate.heartrate.propertyInspectorDidDisappear', (jsonObj) => {
-        console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js]propertyInspectorDidDisappear:');
-    });
-    action.setTitle(data);
+  // Subscribe to the willAppear and other events
+  $SD.on('com.hyperate.hyperate.heartrate.willAppear', (jsonObj) => action.onWillAppear(jsonObj));
+  $SD.on('com.hyperate.hyperate.heartrate.keyUp', (jsonObj) => action.onKeyUp(jsonObj));
+  $SD.on('com.hyperate.hyperate.heartrate.sendToPlugin', (jsonObj) => action.onSendToPlugin(jsonObj));
+  $SD.on('com.hyperate.hyperate.heartrate.didReceiveSettings', (jsonObj) => action.onDidReceiveSettings(jsonObj));
+  $SD.on('com.hyperate.hyperate.heartrate.propertyInspectorDidAppear', (jsonObj) => {
+    console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js]propertyInspectorDidAppear:');
+  });
+  $SD.on('com.hyperate.hyperate.heartrate.propertyInspectorDidDisappear', (jsonObj) => {
+    console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js]propertyInspectorDidDisappear:');
+  });
+  action.setTitle(data);
 };
 
 // ACTIONS
 
 const action = {
-    settings:{},
-    onDidReceiveSettings: function(jsn) {
-        console.log('%c%s', 'color: white; background: red; font-size: 15px;', '[app.js]onDidReceiveSettings:');
+  settings: {},
+  onDidReceiveSettings: function (jsn) {
+    console.log('%c%s', 'color: white; background: red; font-size: 15px;', '[app.js]onDidReceiveSettings:');
 
-        this.settings = Utils.getProp(jsn, 'payload.settings', {});
-        this.doSomeThing(this.settings, 'onDidReceiveSettings', 'orange');
-
-        /**
-         * In this example we put a HTML-input element with id='mynameinput'
-         * into the Property Inspector's DOM. If you enter some data into that
-         * input-field it get's saved to Stream Deck persistently and the plugin
-         * will receive the updated 'didReceiveSettings' event.
-         * Here we look for this setting and use it to change the title of
-         * the key.
-         */
-
-         //this.setTitle(jsn);
-    },
-
-    /** 
-     * The 'willAppear' event is the first event a key will receive, right before it gets
-     * shown on your Stream Deck and/or in Stream Deck software.
-     * This event is a good place to setup your plugin and look at current settings (if any),
-     * which are embedded in the events payload.
-     */
-
-    onWillAppear: function (jsn) {
-        console.log("You can cache your settings in 'onWillAppear'", jsn.payload.settings);
-        /**
-         * The willAppear event carries your saved settings (if any). You can use these settings
-         * to setup your plugin or save the settings for later use. 
-         * If you want to request settings at a later time, you can do so using the
-         * 'getSettings' event, which will tell Stream Deck to send your data 
-         * (in the 'didReceiveSettings above)
-         * 
-         * $SD.api.getSettings(jsn.context);
-        */
-        this.settings = jsn.payload.settings;
-        console.log("looking for setting", this.settings);
-        id = this.settings.HRid;
-        
-        // Nothing in the settings pre-fill, just something for demonstration purposes
-        if (!this.settings || Object.keys(this.settings).length === 0) {
-            this.settings.Code = 'HypeRate';
-        }
-        //this.setTitle(jsn);
-    },
-
-    onKeyUp: function (jsn) {
-        this.doSomeThing(jsn, 'onKeyUp', 'green'); 
-        id = this.settings.HRid;
-        payload = jsn.context;
-        $SD.api.setImage(payload, hrimage);
-        enableHypeRate();
-    },
-
-    onSendToPlugin: function (jsn) {
-        /**
-         * This is a message sent directly from the Property Inspector 
-         * (e.g. some value, which is not saved to settings) 
-         * You can send this event from Property Inspector (see there for an example)
-         */ 
-
-        const sdpi_collection = Utils.getProp(jsn, 'payload.sdpi_collection', {});
-        if (sdpi_collection.value && sdpi_collection.value !== undefined) {
-            this.doSomeThing({ [sdpi_collection.key] : sdpi_collection.value }, 'onSendToPlugin', 'fuchsia');            
-        }
-    },
+    this.settings = Utils.getProp(jsn, 'payload.settings', {});
+    this.doSomeThing(this.settings, 'onDidReceiveSettings', 'orange');
 
     /**
-     * This snippet shows how you could save settings persistantly to Stream Deck software.
-     * It is not used in this example plugin.
+     * In this example we put a HTML-input element with id='mynameinput'
+     * into the Property Inspector's DOM. If you enter some data into that
+     * input-field it get's saved to Stream Deck persistently and the plugin
+     * will receive the updated 'didReceiveSettings' event.
+     * Here we look for this setting and use it to change the title of
+     * the key.
      */
 
-    saveSettings: function (jsn, sdpi_collection) {
-        console.log('saveSettings:', jsn);
-        if (sdpi_collection.hasOwnProperty('key') && sdpi_collection.key != '') {
-            if (sdpi_collection.value && sdpi_collection.value !== undefined) {
-                this.settings[sdpi_collection.key] = sdpi_collection.value;
-                console.log('setSettings....', this.settings);
-                $SD.api.setSettings(jsn.context, this.settings);
-            }
-        }
-    },
+    //this.setTitle(jsn);
+  },
 
+  /** 
+   * The 'willAppear' event is the first event a key will receive, right before it gets
+   * shown on your Stream Deck and/or in Stream Deck software.
+   * This event is a good place to setup your plugin and look at current settings (if any),
+   * which are embedded in the events payload.
+   */
+
+  onWillAppear: function (jsn) {
+    console.log("You can cache your settings in 'onWillAppear'", jsn.payload.settings);
     /**
-     * Here's a quick demo-wrapper to show how you could change a key's title based on what you
-     * stored in settings.
-     * If you enter something into Property Inspector's name field (in this demo),
-     * it will get the title of your key.
+     * The willAppear event carries your saved settings (if any). You can use these settings
+     * to setup your plugin or save the settings for later use. 
+     * If you want to request settings at a later time, you can do so using the
+     * 'getSettings' event, which will tell Stream Deck to send your data 
+     * (in the 'didReceiveSettings above)
      * 
-     * @param {JSON} jsn // The JSON object passed from Stream Deck to the plugin, which contains the plugin's context
-     * 
+     * $SD.api.getSettings(jsn.context);
      */
+    this.settings = jsn.payload.settings;
+    console.log("looking for setting", this.settings);
+    id = this.settings.HRid;
 
-    setTitle: function(hr) {
-        if (this.settings && this.settings.hasOwnProperty('HRid')) {
-            console.log("watch the key on your StreamDeck - it got a new title...", hr);
-        }
-    },
+    // Nothing in the settings pre-fill, just something for demonstration purposes
+    if (!this.settings || Object.keys(this.settings).length === 0) {
+      this.settings.Code = 'HypeRate';
+    }
+    //this.setTitle(jsn);
+  },
 
+  onKeyUp: function (jsn) {
+    this.doSomeThing(jsn, 'onKeyUp', 'green');
+    id = this.settings.HRid;
+    payload = jsn.context;
+    $SD.api.setImage(payload, hrimage);
+    enableHypeRate();
+  },
+
+  onSendToPlugin: function (jsn) {
     /**
-     * Finally here's a method which gets called from various events above.
-     * This is just an idea on how you can act on receiving some interesting message
-     * from Stream Deck.
+     * This is a message sent directly from the Property Inspector 
+     * (e.g. some value, which is not saved to settings) 
+     * You can send this event from Property Inspector (see there for an example)
      */
 
-    doSomeThing: function(inJsonData, caller, tagColor) {
-        console.log('%c%s', `color: white; background: ${tagColor || 'grey'}; font-size: 15px;`, `[app.js]doSomeThing from: ${caller}`);
-        console.log(inJsonData);
-    }, 
+    const sdpi_collection = Utils.getProp(jsn, 'payload.sdpi_collection', {});
+    if (sdpi_collection.value && sdpi_collection.value !== undefined) {
+      this.doSomeThing({
+        [sdpi_collection.key]: sdpi_collection.value
+      }, 'onSendToPlugin', 'fuchsia');
+    }
+  },
+
+  /**
+   * This snippet shows how you could save settings persistantly to Stream Deck software.
+   * It is not used in this example plugin.
+   */
+
+  saveSettings: function (jsn, sdpi_collection) {
+    console.log('saveSettings:', jsn);
+    if (sdpi_collection.hasOwnProperty('key') && sdpi_collection.key != '') {
+      if (sdpi_collection.value && sdpi_collection.value !== undefined) {
+        this.settings[sdpi_collection.key] = sdpi_collection.value;
+        console.log('setSettings....', this.settings);
+        $SD.api.setSettings(jsn.context, this.settings);
+      }
+    }
+  },
+
+  /**
+   * Here's a quick demo-wrapper to show how you could change a key's title based on what you
+   * stored in settings.
+   * If you enter something into Property Inspector's name field (in this demo),
+   * it will get the title of your key.
+   * 
+   * @param {JSON} jsn // The JSON object passed from Stream Deck to the plugin, which contains the plugin's context
+   * 
+   */
+
+  setTitle: function (hr) {
+    if (this.settings && this.settings.hasOwnProperty('HRid')) {
+      console.log("watch the key on your StreamDeck - it got a new title...", hr);
+    }
+  },
+
+  /**
+   * Finally here's a method which gets called from various events above.
+   * This is just an idea on how you can act on receiving some interesting message
+   * from Stream Deck.
+   */
+
+  doSomeThing: function (inJsonData, caller, tagColor) {
+    console.log('%c%s', `color: white; background: ${tagColor || 'grey'}; font-size: 15px;`, `[app.js]doSomeThing from: ${caller}`);
+    console.log(inJsonData);
+  },
 
 
 };
--- Sources/com.hyperate.hyperate.sdPlugin/propertyinspector/js/index_pi.js
+++ Sources/com.hyperate.hyperate.sdPlugin/propertyinspector/js/index_pi.js
@@ -35,12 +35,12 @@ let sdpiWrapper = document.querySelector('.sdpi-wrapper');
 
 let settings;
 
- /**
-  * The 'connected' event is the first event sent to Property Inspector, after it's instance
-  * is registered with Stream Deck software. It carries the current websocket, settings,
-  * and other information about the current environmet in a JSON object.
-  * You can use it to subscribe to events you want to use in your plugin.
-  */
+/**
+ * The 'connected' event is the first event sent to Property Inspector, after it's instance
+ * is registered with Stream Deck software. It carries the current websocket, settings,
+ * and other information about the current environmet in a JSON object.
+ * You can use it to subscribe to events you want to use in your plugin.
+ */
 
 $SD.on('connected', (jsn) => {
     /**
@@ -84,7 +84,7 @@ $SD.on('sendToPropertyInspector', jsn => {
     /**
      *  This is an example, how you could show an error to the user
      */
-     if (pl.hasOwnProperty('error')) {
+    if (pl.hasOwnProperty('error')) {
         sdpiWrapper.innerHTML = `<div class="sdpi-item">
             <details class="message caution">
             <summary class="${pl.hasOwnProperty('info') ? 'pointer' : ''}">${pl.error}</summary>
@@ -118,7 +118,7 @@ const updateUI = (pl) => {
                 }
             }
         }
-   })
+    })
 }
 
 /**
@@ -157,18 +157,33 @@ $SD.on('piDataChanged', (returnValue) => {
 
     console.log('%c%s', 'color: white; background: blue}; font-size: 15px;', 'piDataChanged');
     console.log(returnValue);
-    
+
     if (returnValue.key === 'Save') {
-
-        saveSettings(returnValue);
-
-    } else {
 
         /* SAVE THE VALUE TO SETTINGS */
         saveSettings(returnValue);
 
         /* SEND THE VALUES TO PLUGIN */
         sendValueToPlugin(returnValue, 'sdpi_collection');
+
+    } else if (returnValue.key === 'clickme') {
+
+        postMessage = (w) => {
+            w.postMessage(
+                Object.assign({}, $SD.applicationInfo.application, {
+                    action: $SD.actionInfo.action
+                }), '*');
+        }
+
+        if (!window.xtWindow || window.xtWindow.closed) {
+            window.xtWindow = window.open('../externalWindow.html', 'External Window');
+            setTimeout(() => postMessage(window.xtWindow), 200);
+        } else {
+            postMessage(window.xtWindow);
+        }
+
+    } else {
+        // Nothing
     }
 });
 
@@ -189,7 +204,7 @@ $SD.on('piDataChanged', (returnValue) => {
  *
  */
 
- function saveSettings(sdpi_collection) {
+function saveSettings(sdpi_collection) {
 
     if (typeof sdpi_collection !== 'object') return;
 
@@ -201,21 +216,21 @@ $SD.on('piDataChanged', (returnValue) => {
             $SD.api.setSettings($SD.uuid, settings);
         }
     }
- }
+}
 
- /**
-  * 'sendValueToPlugin' is a wrapper to send some values to the plugin
-  *
-  * It is called with a value and the name of a property:
-  *
-  * sendValueToPlugin(<any value>), 'key-property')
-  *
-  * where 'key-property' is the property you listen for in your plugin's
-  * 'sendToPlugin' events payload.
-  *
-  */
+/**
+ * 'sendValueToPlugin' is a wrapper to send some values to the plugin
+ *
+ * It is called with a value and the name of a property:
+ *
+ * sendValueToPlugin(<any value>), 'key-property')
+ *
+ * where 'key-property' is the property you listen for in your plugin's
+ * 'sendToPlugin' events payload.
+ *
+ */
 
- function sendValueToPlugin(value, prop) {
+function sendValueToPlugin(value, prop) {
     console.log("sendValueToPlugin", value, prop);
     if ($SD.connection && $SD.connection.readyState === 1) {
         const json = {
@@ -266,7 +281,7 @@ function prepareDOMElements(baseElement) {
             if (inputGroup.length === 2) {
                 const offs = inputGroup[0].tagName === 'INPUT' ? 1 : 0;
                 inputGroup[offs].textContent = inputGroup[1 - offs].value;
-                inputGroup[1 - offs]['oninput'] = function() {
+                inputGroup[1 - offs]['oninput'] = function () {
                     inputGroup[offs].textContent = inputGroup[1 - offs].value;
                 };
             }
@@ -276,16 +291,16 @@ function prepareDOMElements(baseElement) {
              */
             Array.from(el.querySelectorAll('.clickable')).forEach(
                 (subel, subi) => {
-                    subel['onclick'] = function(e) {
+                    subel['onclick'] = function (e) {
                         handleSdpiItemChange(e.target, subi);
                     };
                 }
             );
             /** Just in case the found HTML element already has an input or change - event attached,
              * we clone it, and call it in the callback, right before the freshly attached event
-            */
+             */
             const cloneEvt = el[evt];
-            el[evt] = function(e) {
+            el[evt] = function (e) {
                 if (cloneEvt) cloneEvt();
                 handleSdpiItemChange(e.target, i);
             };
@@ -351,9 +366,9 @@ function handleSdpiItemChange(e, idx) {
         // if there's no attribute set for the span, try to see, if there's a value in the textContent
         // and use it as value
         if (!e.hasAttribute('value')) {
-               tmpValue = Number(e.textContent);
+            tmpValue = Number(e.textContent);
             if (typeof tmpValue === 'number' && tmpValue !== null) {
-                e.setAttribute('value', 0+tmpValue); // this is ugly, but setting a value of 0 on a span doesn't do anything
+                e.setAttribute('value', 0 + tmpValue); // this is ugly, but setting a value of 0 on a span doesn't do anything
                 e.value = tmpValue;
             }
         } else {
@@ -369,9 +384,9 @@ function handleSdpiItemChange(e, idx) {
     const isList = ['LI', 'OL', 'UL', 'DL', 'TD'].includes(e.tagName);
     const sdpiItem = e.closest('.sdpi-item');
     const sdpiItemGroup = e.closest('.sdpi-item-group');
-    let sdpiItemChildren = isList
-        ? sdpiItem.querySelectorAll(e.tagName === 'LI' ? 'li' : 'td')
-        : sdpiItem.querySelectorAll('.sdpi-item-child > input');
+    let sdpiItemChildren = isList ?
+        sdpiItem.querySelectorAll(e.tagName === 'LI' ? 'li' : 'td') :
+        sdpiItem.querySelectorAll('.sdpi-item-child > input');
 
     if (isList) {
         const siv = e.closest('.sdpi-item-value');
@@ -383,7 +398,7 @@ function handleSdpiItemChange(e, idx) {
         }
     }
 
-    if (sdpiItemChildren.length && ['radio','checkbox'].includes(sdpiItemChildren[0].type)) {
+    if (sdpiItemChildren.length && ['radio', 'checkbox'].includes(sdpiItemChildren[0].type)) {
         e.setAttribute('_value', e.checked); //'_value' has priority over .value
     }
     if (sdpiItemGroup && !sdpiItemChildren.length) {
@@ -396,7 +411,9 @@ function handleSdpiItemChange(e, idx) {
     if (e.selectedIndex !== undefined) {
         if (e.tagName === 'SELECT') {
             sdpiItemChildren.forEach((ec, i) => {
-                selectedElements.push({ [ec.id]: ec.value });
+                selectedElements.push({
+                    [ec.id]: ec.value
+                });
             });
         }
         idx = e.selectedIndex;
@@ -414,15 +431,15 @@ function handleSdpiItemChange(e, idx) {
 
     const returnValue = {
         key: e.id && e.id.charAt(0) !== '_' ? e.id : sdpiItem.id,
-        value: isList
-            ? e.textContent
-            : e.hasAttribute('_value')
-            ? e.getAttribute('_value')
-            : e.value
-            ? e.type === 'file'
-                ? decodeURIComponent(e.value.replace(/^C:\\fakepath\\/, ''))
-                : e.value
-            : e.getAttribute('value'),
+        value: isList ?
+            e.textContent :
+            e.hasAttribute('_value') ?
+            e.getAttribute('_value') :
+            e.value ?
+            e.type === 'file' ?
+            decodeURIComponent(e.value.replace(/^C:\\fakepath\\/, '')) :
+            e.value :
+            e.getAttribute('value'),
         group: sdpiItemGroup ? sdpiItemGroup.id : false,
         index: idx,
         selection: selectedElements,
@@ -437,11 +454,11 @@ function handleSdpiItemChange(e, idx) {
         const info = sdpiItem.querySelector('.sdpi-file-info');
         if (info) {
             const s = returnValue.value.split('/').pop();
-            info.textContent =                s.length > 28
-                    ? s.substr(0, 10)
-                      + '...'
-                      + s.substr(s.length - 10, s.length)
-                    : s;
+            info.textContent = s.length > 28 ?
+                s.substr(0, 10) +
+                '...' +
+                s.substr(s.length - 10, s.length) :
+                s;
         }
     }
 
@@ -467,10 +484,10 @@ function localizeUI() {
     });
     Array.from(el.querySelectorAll('*:not(script)')).forEach(e => {
         if (
-            e.childNodes
-            && e.childNodes.length > 0
-            && e.childNodes[0].nodeValue
-            && typeof e.childNodes[0].nodeValue === 'string'
+            e.childNodes &&
+            e.childNodes.length > 0 &&
+            e.childNodes[0].nodeValue &&
+            typeof e.childNodes[0].nodeValue === 'string'
         ) {
             t = e.childNodes[0].nodeValue.lox();
             if (e.childNodes[0].nodeValue !== t) {
@@ -486,7 +503,7 @@ function localizeUI() {
  *
  */
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     document.body.classList.add(navigator.userAgent.includes("Mac") ? 'mac' : 'win');
     prepareDOMElements();
     $SD.on('localizationLoaded', (language) => {
@@ -495,7 +512,7 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 /** the beforeunload event is fired, right before the PI will remove all nodes */
-window.addEventListener('beforeunload', function(e) {
+window.addEventListener('beforeunload', function (e) {
     e.preventDefault();
     sendValueToPlugin('propertyInspectorWillDisappear', 'property_inspector');
     // Don't set a returnValue to the event, otherwise Chromium with throw an error.  // e.returnValue = '';
