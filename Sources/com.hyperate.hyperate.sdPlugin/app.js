5a313cc4761ab290e2de4c0659b9c457b938b6ac
IanChandler <cell600@hotmail.com>
2023-03-21 12:50:00 +0000

    Update v2.5.1

--- Sources/com.hyperate.hyperate.sdPlugin/app.js
+++ Sources/com.hyperate.hyperate.sdPlugin/app.js
@@ -13,263 +13,262 @@ var start;
 var millis;
 var seconds = 0;
 var WSstate = "CLOSED"
-
+var autoreconnect = false;
 
 function Cleartime() {
-  millis = "0";
-  seconds = "0";
+    millis = "0";
+    seconds = "0";
 }
 
 function DisconnectHypeRate() {
-  hypeRateSocket.close();
-  clearInterval(heartbeatInterval);
-  WSstate = "CLOSED"
-  $SD.api.setImage(payload, HRimgbase);
+    hypeRateSocket.close();
+    clearInterval(heartbeatInterval);
+    WSstate = "CLOSED"
+    $SD.api.setImage(payload, HRimgbase);
 
 }
-function enableHypeRate() {
-  const hypeRateURL =
-    "wss://app.hyperate.io/socket/websocket?token=" + apikey;
-  window.hypeRateSocket = new WebSocket(hypeRateURL);
-        // Set hearbeat interval on connect
-        hypeRateSocket.onopen = () => {
-          console.log(`HypeRate WebSocket - Connected`);
-          heartbeatInterval = setInterval(heartbeat, 10000);
-          connectToHypeRate(id);
-          WSstate = "connected"
-        };
 
-        function heartbeat() {
-          hypeRateSocket.send(JSON.stringify({
+function enableHypeRate() {
+    const hypeRateURL =
+        "wss://app.hyperate.io/socket/websocket?token=" + apikey;
+    window.hypeRateSocket = new WebSocket(hypeRateURL);
+    // Set hearbeat interval on connect
+    hypeRateSocket.onopen = () => {
+        console.log(`HypeRate WebSocket - Connected`);
+        heartbeatInterval = setInterval(heartbeat, 10000);
+        connectToHypeRate(id);
+        WSstate = "connected"
+    };
+
+    function heartbeat() {
+        hypeRateSocket.send(JSON.stringify({
             topic: "phoenix",
             event: "heartbeat",
             payload: {},
             ref: 0,
-          }));
-        }
-  // Handle incoming message
-  window.hypeRateSocket.onmessage = (event) => {
-    let eventData = JSON.parse(event.data);
-    if (eventData.event !== "hr_update") {
-      return;
+        }));
     }
-    console.log(eventData.payload.hr);
-    makeimage(eventData.payload.hr);
-  };
-  // Reconnect on disconnect
-  window.hypeRateSocket.onclose = function (event) {
-    console.error(`HypeRate Error: ${event.code} ${event.reason}`);
-    //enableHypeRate();
-  };
+    // Handle incoming message
+    window.hypeRateSocket.onmessage = (event) => {
+        let eventData = JSON.parse(event.data);
+        if (eventData.event !== "hr_update") {
+            return;
+        }
+        makeimage(eventData.payload.hr);
+    };
+    // Reconnect on disconnect
+    window.hypeRateSocket.onclose = function(event) {
+        console.error(`HypeRate Error: ${event.code} ${event.reason}`);
+        if (autoreconnect) {
+            enableHypeRate();
+        } else {
+            console.error("Reconnect Set to False");
+        }
+        enableHypeRate();
+    };
 }
 async function connectToHypeRate(sessionID) {
-  console.log(`HypeRate - Joining ${sessionID}`);
-  joinHypeRateChannel();
+    console.log(`HypeRate - Joining ${sessionID}`);
+    joinHypeRateChannel();
 }
 // Join the HypeRate Id on WebSocket
 async function joinHypeRateChannel() {
-  window.hypeRateSocket.send(
-    JSON.stringify({
-      topic: `hr:${id}`,
-      event: "phx_join",
-      payload: {},
-      ref: 0,
-    })
-  );
+    window.hypeRateSocket.send(
+        JSON.stringify({
+            topic: `hr:${id}`,
+            event: "phx_join",
+            payload: {},
+            ref: 0,
+        })
+    );
 }
 
 function makeimage(heartrate) {
 
-  let elem = document.querySelector("canvas");
-  let ctx = elem.getContext("2d");
-  
-  var image = new Image();
-  image.onload = function () {
-    ctx.drawImage(image, 0, 0);
-    ctx.font = "bold 50px Arial";
-  
-    let pulse = heartrate;
-    let measurements = ctx.measureText(pulse);
-  
-    console.log(measurements);
-  
-    let text_width = measurements.width;
-    let text_height = measurements;
-  
-    console.log(text_width);
-  
-    let canvas_width = elem.width;
-    let canvas_height = elem.height;
-  
-    ctx.fillStyle = "white";
-    ctx.fillText(pulse, (canvas_width - text_width) / 2, canvas_height / 2 + 15);
+    let elem = document.querySelector("canvas");
+    let ctx = elem.getContext("2d");
 
-    ctx.fillStyle = "white";
-    ctx.font = "bold 30px Arial";
-    ctx.fillText("BPM", 42, 120);
-  
-    // Result here
-    console.log(elem.toDataURL());
-    streamdeckimage = elem.toDataURL();
-    $SD.api.setImage(payload, streamdeckimage);
-  };
-  image.src = hrimage;
-  }
+    var image = new Image();
+    image.onload = function() {
+        ctx.drawImage(image, 0, 0);
+        ctx.font = "bold 50px Arial";
+
+        let pulse = heartrate;
+        let measurements = ctx.measureText(pulse);
+
+        let text_width = measurements.width;
+        let text_height = measurements;
+
+        let canvas_width = elem.width;
+        let canvas_height = elem.height;
+
+        ctx.fillStyle = "white";
+        ctx.fillText(pulse, (canvas_width - text_width) / 2, canvas_height / 2 + 15);
+
+        ctx.fillStyle = "white";
+        ctx.font = "bold 30px Arial";
+        ctx.fillText("BPM", 42, 120);
+
+        // Result here
+        streamdeckimage = elem.toDataURL();
+        $SD.api.setImage(payload, streamdeckimage);
+    };
+    image.src = hrimage;
+}
 
 $SD.on('connected', (jsonObj) => connected(jsonObj));
 
 function connected(data) {
-  // Subscribe to the willAppear and other events
-  $SD.on('com.hyperate.hyperate.heartrate.willAppear', (jsonObj) => action.onWillAppear(jsonObj));
-  $SD.on('com.hyperate.hyperate.heartrate.keyUp', (jsonObj) => action.onKeyUp(jsonObj));
-  $SD.on('com.hyperate.hyperate.heartrate.keyDown', (jsonObj) => action.onkeyDown(jsonObj));
-  $SD.on('com.hyperate.hyperate.heartrate.sendToPlugin', (jsonObj) => action.onSendToPlugin(jsonObj));
-  $SD.on('com.hyperate.hyperate.heartrate.didReceiveSettings', (jsonObj) => action.onDidReceiveSettings(jsonObj));
-  $SD.on('com.hyperate.hyperate.heartrate.propertyInspectorDidAppear', (jsonObj) => {
-    console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js]propertyInspectorDidAppear:');
-  });
-  $SD.on('com.hyperate.hyperate.heartrate.propertyInspectorDidDisappear', (jsonObj) => {
-    console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js]propertyInspectorDidDisappear:');
-  });
+    // Subscribe to the willAppear and other events
+    $SD.on('com.hyperate.hyperate.heartrate.willAppear', (jsonObj) => action.onWillAppear(jsonObj));
+    $SD.on('com.hyperate.hyperate.heartrate.keyUp', (jsonObj) => action.onKeyUp(jsonObj));
+    $SD.on('com.hyperate.hyperate.heartrate.keyDown', (jsonObj) => action.onkeyDown(jsonObj));
+    $SD.on('com.hyperate.hyperate.heartrate.sendToPlugin', (jsonObj) => action.onSendToPlugin(jsonObj));
+    $SD.on('com.hyperate.hyperate.heartrate.didReceiveSettings', (jsonObj) => action.onDidReceiveSettings(jsonObj));
+    $SD.on('com.hyperate.hyperate.heartrate.propertyInspectorDidAppear', (jsonObj) => {
+        console.log('%c%s', 'color: white; background: black; font-size: 13px;', '[app.js]propertyInspectorDidAppear:');
+    });
+    $SD.on('com.hyperate.hyperate.heartrate.propertyInspectorDidDisappear', (jsonObj) => {
+        console.log('%c%s', 'color: white; background: red; font-size: 13px;', '[app.js]propertyInspectorDidDisappear:');
+    });
 };
 
 // ACTIONS
 
 const action = {
-  settings: {},
-  onDidReceiveSettings: function (jsn) {
-    console.log('%c%s', 'color: white; background: red; font-size: 15px;', '[app.js]onDidReceiveSettings:');
+    settings: {},
+    onDidReceiveSettings: function(jsn) {
+        console.log('%c%s', 'color: white; background: red; font-size: 15px;', '[app.js]onDidReceiveSettings:');
 
-    this.settings = Utils.getProp(jsn, 'payload.settings', {});
-    this.doSomeThing(this.settings, 'onDidReceiveSettings', 'orange');
+        this.settings = Utils.getProp(jsn, 'payload.settings', {});
+        this.doSomeThing(this.settings, 'onDidReceiveSettings', 'orange');
 
-    /**
-     * In this example we put a HTML-input element with id='mynameinput'
-     * into the Property Inspector's DOM. If you enter some data into that
-     * input-field it get's saved to Stream Deck persistently and the plugin
-     * will receive the updated 'didReceiveSettings' event.
-     * Here we look for this setting and use it to change the title of
-     * the key.
+        /**
+         * In this example we put a HTML-input element with id='mynameinput'
+         * into the Property Inspector's DOM. If you enter some data into that
+         * input-field it get's saved to Stream Deck persistently and the plugin
+         * will receive the updated 'didReceiveSettings' event.
+         * Here we look for this setting and use it to change the title of
+         * the key.
+         */
+    },
+
+    /** 
+     * The 'willAppear' event is the first event a key will receive, right before it gets
+     * shown on your Stream Deck and/or in Stream Deck software.
+     * This event is a good place to setup your plugin and look at current settings (if any),
+     * which are embedded in the events payload.
      */
-  },
 
-  /** 
-   * The 'willAppear' event is the first event a key will receive, right before it gets
-   * shown on your Stream Deck and/or in Stream Deck software.
-   * This event is a good place to setup your plugin and look at current settings (if any),
-   * which are embedded in the events payload.
-   */
+    onWillAppear: function(jsn) {
+        console.log("You can cache your settings in 'onWillAppear'", jsn.payload.settings);
+        /**
+         * The willAppear event carries your saved settings (if any). You can use these settings
+         * to setup your plugin or save the settings for later use. 
+         * If you want to request settings at a later time, you can do so using the
+         * 'getSettings' event, which will tell Stream Deck to send your data 
+         * (in the 'didReceiveSettings above)
+         * 
+         * $SD.api.getSettings(jsn.context);
+         */
+        this.settings = jsn.payload.settings;
+        console.log("looking for setting", this.settings);
+        id = this.settings.HRid;
 
-  onWillAppear: function (jsn) {
-    console.log("You can cache your settings in 'onWillAppear'", jsn.payload.settings);
-    /**
-     * The willAppear event carries your saved settings (if any). You can use these settings
-     * to setup your plugin or save the settings for later use. 
-     * If you want to request settings at a later time, you can do so using the
-     * 'getSettings' event, which will tell Stream Deck to send your data 
-     * (in the 'didReceiveSettings above)
-     * 
-     * $SD.api.getSettings(jsn.context);
-     */
-    this.settings = jsn.payload.settings;
-    console.log("looking for setting", this.settings);
-    id = this.settings.HRid;
+        // Nothing in the settings pre-fill, just something for demonstration purposes
+        if (!this.settings || Object.keys(this.settings).length === 0) {
+            this.settings.Code = 'HypeRate';
+        }
+    },
 
-    // Nothing in the settings pre-fill, just something for demonstration purposes
-    if (!this.settings || Object.keys(this.settings).length === 0) {
-      this.settings.Code = 'HypeRate';
-    }
-  },
-
-  onKeyUp: function (jsn) {
-    this.doSomeThing(jsn, 'onKeyUp', 'green');
-    id = this.settings.HRid;
-    payload = jsn.context;
-    //enableHypeRate();
+    onKeyUp: function(jsn) {
+        this.doSomeThing(jsn, 'onKeyUp', 'green');
+        id = this.settings.HRid;
+        payload = jsn.context;
 
         keyDown = false;
         keyUp = true;
         millis = Date.now() - start;
         seconds = Math.floor(millis / 1000);
-      
-      if (seconds > 5) {
-        console.log("LongPress");
-        setTimeout(Cleartime, 2000);
-        DisconnectHypeRate();
-      }
-      if (seconds < 5 && WSstate == "CLOSED") {
-        console.log("Shortpress");
-        setTimeout(Cleartime, 2000);
-        enableHypeRate();
-      }
-    ;
-  },
 
-  onkeyDown: function (jsn) {
-    this.doSomeThing(jsn, 'onKeydown', 'green');
-    
-      allowed = false;
-      keyDown = true;
-      keyUp = false;
-      start = Date.now();
-  },
-  onSendToPlugin: function (jsn) {
+        if (seconds > 5) {
+            console.log("LongPress");
+            setTimeout(Cleartime, 2000);
+            autoreconnect = false;
+            DisconnectHypeRate();
+        }
+        if (seconds < 5 && WSstate == "CLOSED") {
+            console.log("Shortpress");
+            setTimeout(Cleartime, 2000);
+            autoreconnect = true;
+            enableHypeRate();
+        };
+    },
+
+    onkeyDown: function(jsn) {
+        this.doSomeThing(jsn, 'onKeydown', 'green');
+
+        allowed = false;
+        keyDown = true;
+        keyUp = false;
+        start = Date.now();
+    },
+    onSendToPlugin: function(jsn) {
+        /**
+         * This is a message sent directly from the Property Inspector 
+         * (e.g. some value, which is not saved to settings) 
+         * You can send this event from Property Inspector (see there for an example)
+         */
+
+        const sdpi_collection = Utils.getProp(jsn, 'payload.sdpi_collection', {});
+        if (sdpi_collection.value && sdpi_collection.value !== undefined) {
+            this.doSomeThing({
+                [sdpi_collection.key]: sdpi_collection.value
+            }, 'onSendToPlugin', 'fuchsia');
+        }
+    },
+
     /**
-     * This is a message sent directly from the Property Inspector 
-     * (e.g. some value, which is not saved to settings) 
-     * You can send this event from Property Inspector (see there for an example)
+     * This snippet shows how you could save settings persistantly to Stream Deck software.
+     * It is not used in this example plugin.
      */
 
-    const sdpi_collection = Utils.getProp(jsn, 'payload.sdpi_collection', {});
-    if (sdpi_collection.value && sdpi_collection.value !== undefined) {
-      this.doSomeThing({
-        [sdpi_collection.key]: sdpi_collection.value
-      }, 'onSendToPlugin', 'fuchsia');
-    }
-  },
+    saveSettings: function(jsn, sdpi_collection) {
+        console.log('saveSettings:', jsn);
+        if (sdpi_collection.hasOwnProperty('key') && sdpi_collection.key != '') {
+            if (sdpi_collection.value && sdpi_collection.value !== undefined) {
+                this.settings[sdpi_collection.key] = sdpi_collection.value;
+                console.log('setSettings....', this.settings);
+                $SD.api.setSettings(jsn.context, this.settings);
+            }
+        }
+    },
 
-  /**
-   * This snippet shows how you could save settings persistantly to Stream Deck software.
-   * It is not used in this example plugin.
-   */
+    /**
+     * Here's a quick demo-wrapper to show how you could change a key's title based on what you
+     * stored in settings.
+     * If you enter something into Property Inspector's name field (in this demo),
+     * it will get the title of your key.
+     * 
+     * @param {JSON} jsn // The JSON object passed from Stream Deck to the plugin, which contains the plugin's context
+     * 
+     */
 
-  saveSettings: function (jsn, sdpi_collection) {
-    console.log('saveSettings:', jsn);
-    if (sdpi_collection.hasOwnProperty('key') && sdpi_collection.key != '') {
-      if (sdpi_collection.value && sdpi_collection.value !== undefined) {
-        this.settings[sdpi_collection.key] = sdpi_collection.value;
-        console.log('setSettings....', this.settings);
-        $SD.api.setSettings(jsn.context, this.settings);
-      }
-    }
-  },
+    setTitle: function(hr) {
+        if (this.settings && this.settings.hasOwnProperty('HRid')) {
+            console.log("watch the key on your StreamDeck - it got a new title...", hr);
+        }
+    },
 
-  /**
-   * Here's a quick demo-wrapper to show how you could change a key's title based on what you
-   * stored in settings.
-   * If you enter something into Property Inspector's name field (in this demo),
-   * it will get the title of your key.
-   * 
-   * @param {JSON} jsn // The JSON object passed from Stream Deck to the plugin, which contains the plugin's context
-   * 
-   */
+    /**
+     * Finally here's a method which gets called from various events above.
+     * This is just an idea on how you can act on receiving some interesting message
+     * from Stream Deck.
+     */
 
-  setTitle: function (hr) {
-    if (this.settings && this.settings.hasOwnProperty('HRid')) {
-      console.log("watch the key on your StreamDeck - it got a new title...", hr);
-    }
-  },
-
-  /**
-   * Finally here's a method which gets called from various events above.
-   * This is just an idea on how you can act on receiving some interesting message
-   * from Stream Deck.
-   */
-
-  doSomeThing: function (inJsonData, caller, tagColor) {
-    console.log('%c%s', `color: white; background: ${tagColor || 'grey'}; font-size: 15px;`, `[app.js]doSomeThing from: ${caller}`);
-    console.log(inJsonData);
-  },
-
+    doSomeThing: function(inJsonData, caller, tagColor) {
+        console.log('%c%s', `color: white; background: ${tagColor || 'grey'}; font-size: 15px;`, `[app.js]doSomeThing from: ${caller}`);
+        console.log(inJsonData);
+    },
 
 };
