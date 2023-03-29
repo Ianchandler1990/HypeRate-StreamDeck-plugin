6caec95f9a6ce3592dc59d94ecae10914bab8f26
IanChandler <cell600@hotmail.com>
2023-03-29 23:21:19 +0100

    Update app.js

--- Sources/com.hyperate.hyperate.sdPlugin/app.js
+++ Sources/com.hyperate.hyperate.sdPlugin/app.js
@@ -64,7 +64,6 @@ function enableHypeRate() {
         } else {
             console.error("Reconnect Set to False");
         }
-        enableHypeRate();
     };
 }
 async function connectToHypeRate(sessionID) {
@@ -142,43 +141,13 @@ const action = {
 
         this.settings = Utils.getProp(jsn, 'payload.settings', {});
         this.doSomeThing(this.settings, 'onDidReceiveSettings', 'orange');
-
-        /**
-         * In this example we put a HTML-input element with id='mynameinput'
-         * into the Property Inspector's DOM. If you enter some data into that
-         * input-field it get's saved to Stream Deck persistently and the plugin
-         * will receive the updated 'didReceiveSettings' event.
-         * Here we look for this setting and use it to change the title of
-         * the key.
-         */
     },
-
-    /** 
-     * The 'willAppear' event is the first event a key will receive, right before it gets
-     * shown on your Stream Deck and/or in Stream Deck software.
-     * This event is a good place to setup your plugin and look at current settings (if any),
-     * which are embedded in the events payload.
-     */
 
     onWillAppear: function(jsn) {
         console.log("You can cache your settings in 'onWillAppear'", jsn.payload.settings);
-        /**
-         * The willAppear event carries your saved settings (if any). You can use these settings
-         * to setup your plugin or save the settings for later use. 
-         * If you want to request settings at a later time, you can do so using the
-         * 'getSettings' event, which will tell Stream Deck to send your data 
-         * (in the 'didReceiveSettings above)
-         * 
-         * $SD.api.getSettings(jsn.context);
-         */
         this.settings = jsn.payload.settings;
         console.log("looking for setting", this.settings);
         id = this.settings.HRid;
-
-        // Nothing in the settings pre-fill, just something for demonstration purposes
-        if (!this.settings || Object.keys(this.settings).length === 0) {
-            this.settings.Code = 'HypeRate';
-        }
     },
 
     onKeyUp: function(jsn) {
@@ -214,12 +183,6 @@ const action = {
         start = Date.now();
     },
     onSendToPlugin: function(jsn) {
-        /**
-         * This is a message sent directly from the Property Inspector 
-         * (e.g. some value, which is not saved to settings) 
-         * You can send this event from Property Inspector (see there for an example)
-         */
-
         const sdpi_collection = Utils.getProp(jsn, 'payload.sdpi_collection', {});
         if (sdpi_collection.value && sdpi_collection.value !== undefined) {
             this.doSomeThing({
@@ -227,11 +190,6 @@ const action = {
             }, 'onSendToPlugin', 'fuchsia');
         }
     },
-
-    /**
-     * This snippet shows how you could save settings persistantly to Stream Deck software.
-     * It is not used in this example plugin.
-     */
 
     saveSettings: function(jsn, sdpi_collection) {
         console.log('saveSettings:', jsn);
@@ -244,27 +202,11 @@ const action = {
         }
     },
 
-    /**
-     * Here's a quick demo-wrapper to show how you could change a key's title based on what you
-     * stored in settings.
-     * If you enter something into Property Inspector's name field (in this demo),
-     * it will get the title of your key.
-     * 
-     * @param {JSON} jsn // The JSON object passed from Stream Deck to the plugin, which contains the plugin's context
-     * 
-     */
-
     setTitle: function(hr) {
         if (this.settings && this.settings.hasOwnProperty('HRid')) {
             console.log("watch the key on your StreamDeck - it got a new title...", hr);
         }
     },
-
-    /**
-     * Finally here's a method which gets called from various events above.
-     * This is just an idea on how you can act on receiving some interesting message
-     * from Stream Deck.
-     */
 
     doSomeThing: function(inJsonData, caller, tagColor) {
         console.log('%c%s', `color: white; background: ${tagColor || 'grey'}; font-size: 15px;`, `[app.js]doSomeThing from: ${caller}`);
